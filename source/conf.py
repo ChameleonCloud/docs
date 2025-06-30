18a948a8aa768f92fdc00c0598078d1bf399965b
Mark Powers <markpowers@uchicago.edu>
2025-06-30 09:35:27 -0500

    Update sphinx and dependencies
    This fixes some warnings during build time due to issues in python 3.13
    that are fixed in newer sphinx versions. Sphinx itself was being held
    back by the pinning of docutils.

--- source/conf.py
+++ source/conf.py
@@ -33,8 +33,6 @@ sys.path.insert(0, os.path.abspath('_ext'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
     'sphinx.ext.githubpages'
 ]
 
@@ -69,7 +67,7 @@ release = '2.0a'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -204,7 +202,6 @@ html_context = {
     "github_version": "master",
     "conf_py_path": "/source/",
 }
-
 
 
 def setup(app):
